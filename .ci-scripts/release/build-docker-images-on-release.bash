69ba40c7b3672688c151f47dc6dd637d777c84a0
Sean T. Allen <sean@seantallen.com>
2019-11-10 03:06:32 -0500

    Add automated release process
    Adds our standard GitHub based automated release process and associated
    scripts.
    
    Includes
    
    - removal of CircleCI
    - nightly releases to Cloudsmith
    - tagged releases to Cloudsmith
    - "latest" tag Docker images to DockerHub
    - "release" and "X.Y.Z" tag Docker images to DockerHub
    - changelog-bot support
    - PR CI workflow
    - Release process instructions
    
    Closes #41

---
+++ .ci-scripts/release/build-docker-images-on-release.bash
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# *** You should already be logged in to DockerHub when you run this ***
+#
+# Builds docker release images with two tags:
+#
+# - release
+# - X.Y.Z for example 0.32.1
+#
+# Tools required in the environment that runs this:
+#
+# - bash
+# - docker
+
+set -o errexit
+
+# Pull in shared configuration specific to this repo
+base=$(dirname "$0")
+source "${base}/config.bash"
+
+# Verify ENV is set up correctly
+# We validate all that need to be set in case, in an absolute emergency,
+# we need to run this by hand. Otherwise the GitHub actions environment should
+# provide all of these if properly configured
+if [[ -z "${GITHUB_REF}" ]]; then
+  echo -e "\e[31mThe release tag needs to be set in GITHUB_REF."
+  echo -e "\e[31mThe tag should be in the following GitHub specific form:"
+  echo -e "\e[31m    /refs/tags/X.Y.Z"
+  echo -e "\e[31mwhere X.Y.Z is the version we are releasing"
+  echo -e "\e[31mExiting.\e[0m"
+  exit 1
+fi
+
+if [[ -z "${GITHUB_REPOSITORY}" ]]; then
+  echo -e "\e[31mName of this repository needs to be set in GITHUB_REPOSITORY."
+  echo -e "\e[31mShould be in the form OWNER/REPO, for example:"
+  echo -e "\e[31m     ponylang/ponyup"
+  echo -e "\e[31mThis will be used as the docker image name."
+  echo -e "\e[31mExiting.\e[0m"
+  exit 1
+fi
+
+# no unset variables allowed from here on out
+# allow above so we can display nice error messages for expected unset variables
+set -o nounset
+
+# We aren't validating TAG is in our x.y.z format but we could.
+# For now, TAG validating is left up to the configuration in
+# our GitHub workflow
+# Tag ref version: "refs/tags/1.0.0"
+# Version: "1.0.0"
+VERSION="${GITHUB_REF/refs\/tags\//}"
+
+# Build and push :VERSION tag e.g. ponylang/ponyup:0.32.1
+DOCKER_TAG=${GITHUB_REPOSITORY}:"${VERSION}"
+docker build -t "${DOCKER_TAG}" .
+docker push "${DOCKER_TAG}"
+
+# Build and push "release" tag e.g. ponylang/ponyup:release
+DOCKER_TAG=${GITHUB_REPOSITORY}:release
+docker build -t "${DOCKER_TAG}" .
+docker push "${DOCKER_TAG}"
