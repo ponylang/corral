9f585c4f19367ebf4fc9208029c5ef95d3b64a33
Sean T. Allen <sean@seantallen.com>
2020-05-06 20:18:05 -0400

    Fix broken macOS corral dependencies

--- .ci-scripts/macOS-install-pony-tools.bash
+++ .ci-scripts/macOS-install-pony-tools.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#
+# Libressl is required by ponyup
+#
+
+brew install libressl
+
+#
+# Install ponyup
+#
+
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh | sh
 
 export PATH="$HOME/.local/share/ponyup/bin/:$PATH"
