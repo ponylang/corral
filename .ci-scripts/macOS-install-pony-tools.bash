b9a7554734f908d8ef7f76fab966f473bf14f686
Sean T. Allen <sean@seantallen.com>
2021-07-12 20:57:40 -0400

    Switch nightlies back to being built with the release ponyc

--- .ci-scripts/macOS-install-pony-tools.bash
+++ .ci-scripts/macOS-install-pony-tools.bash
@@ -16,4 +16,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/
 
 export PATH="$HOME/.local/share/ponyup/bin/:$PATH"
 
-ponyup update ponyc nightly
+ponyup update ponyc release
